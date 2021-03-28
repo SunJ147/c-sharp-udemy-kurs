7415df29ee07efda9e341dcae3e81c3cb7e9681a
Sonja Milde <sonja.milde@outlook.com>
2021-03-28 22:49:28 +0200

    Klasse AnwendungsController erstellt und restliches ausgelagert.

---
+++ Udemy_cs_Kurs/AnwendungsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_cs_Kurs
+{
+    class AnwendungsController
+    {
+        private ConsoleView view;
+        private RechnerModel model;
+
+        public AnwendungsController(ConsoleView view, RechnerModel model)
+        {
+            this.view = view;
+            this.model = model;
+        }
+
+        public void Ausfuehren()
+        {
+            string ersteZahlAlsString = view.HoleZahlVonBenutzer();
+            string operation = view.HoleOperatorVonBenutzer();
+            string zweiteZahlAlsString = view.HoleZahlVonBenutzer();
+
+            double ersteZahl = KonvertierungStringInDouble(ersteZahlAlsString);
+            double zweiteZahl = KonvertierungStringInDouble(zweiteZahlAlsString);
+
+            model.FuehreRechnungAus(ersteZahl, zweiteZahl, operation);
+            view.GebeErgebnisAus(operation);
+            view.WarteAufEndeVonBenutzer();
+        }
+
+        static double KonvertierungStringInDouble(string zahl)
+        {
+            return Convert.ToDouble(zahl);
+        }
+    }
+}
--- Udemy_cs_Kurs/Program.cs
+++ Udemy_cs_Kurs/Program.cs
@@ -9,57 +9,12 @@ namespace Udemy_cs_Kurs
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             RechnerModel model = new RechnerModel();
             ConsoleView view = new ConsoleView(model);
-            string ersteZahlAlsString = view.HoleZahlVonBenutzer();
-            string operation = view.HoleOperatorVonBenutzer();
-            string zweiteZahlAlsString = view.HoleZahlVonBenutzer();
-            
+            AnwendungsController controller = new AnwendungsController(view, model);
 
-            double ersteZahl = KonvertierungStringInDouble(ersteZahlAlsString);
-            double zweiteZahl = KonvertierungStringInDouble(zweiteZahlAlsString);
-
-            
-            model.FuehreRechnungAus(ersteZahl, zweiteZahl, operation);
-            view.GebeErgebnisAus(operation);
-            view.WarteAufEndeVonBenutzer();
+            controller.Ausfuehren();
         }
-
-        static void GebeErgebnisAus(double ergebnis, string operation)
-        {
-            switch (operation)
-            {
-                case "+": 
-                    Console.WriteLine($"Die Summe ist: {ergebnis}"); 
-                    Console.ReadLine();
-                    break;
-                case "-": 
-                    Console.WriteLine($"Die Differenz ist: {ergebnis}"); 
-                    Console.ReadLine();
-                    break;
-                case "*": 
-                    Console.WriteLine($"Die Multiplikation ergibt: {ergebnis}"); 
-                    Console.ReadLine();
-                    break;
-                case "/": 
-                    Console.WriteLine($"Die Division ergibt: {ergebnis}"); 
-                    Console.ReadLine();
-                    break;
-            }
-            return;
-        }
-
-        static double KonvertierungStringInDouble(string zahl)
-        {
-            return Convert.ToDouble(zahl);
-        }
-
-        static string HoleBenutzereingabe(string konsolenAusgabe)
-        {
-            Console.Write(konsolenAusgabe);
-            return Console.ReadLine();
-        }
-        
     }
 }
